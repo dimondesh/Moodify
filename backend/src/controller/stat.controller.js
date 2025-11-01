67a5f6fc0b1b6259f1423ec7ee97634f586a8d90
dimondesh <dimondesh1337@gmail.com>
2025-11-01 03:59:38 +0200

    status of backend and audio analysis service endpoint added

--- backend/src/controller/stat.controller.js
+++ backend/src/controller/stat.controller.js
@@ -2,6 +2,8 @@ import { Song } from "../models/song.model.js";
 import { User } from "../models/user.model.js";
 import { Album } from "../models/album.model.js";
 import { Artist } from "../models/artist.model.js";
+import https from "https";
+import axios from "axios";
 
 export const getStats = async (req, res, next) => {
   try {
@@ -21,5 +23,33 @@ export const getStats = async (req, res, next) => {
     });
   } catch (error) {
     next(error);
+  }
+};
+export const getHealthStatus = async (req, res, next) => {
+  try {
+    res.status(200).json({
+      status: "ok",
+      message: "Backend is awake and running.",
+      timestamp: new Date().toISOString(),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+export const checkAnalysisServiceHealth = async (req, res, next) => {
+  try {
+    const analysisServiceUrl = "https://moodify-analysis-service.onrender.com";
+
+    const agent = new https.Agent({
+      rejectUnauthorized: false,
+    });
+
+    const response = await axios.get(analysisServiceUrl, { httpsAgent: agent });
+
+    res.status(response.status).json(response.data);
+  } catch (error) {
+    console.error("Analysis service check failed:", error);
+
+    res.status(502).json({ error: "Analysis service is unreachable" });
   }
 };
--- backend/src/routes/stat.route.js
+++ backend/src/routes/stat.route.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getStats } from "../controller/stat.controller.js";
+import {
+  getStats,
+  getHealthStatus,
+  checkAnalysisServiceHealth,
+} from "../controller/stat.controller.js";
 
 const router = Router();
 
 router.get("/", protectRoute, getStats);
+router.get("/health", getHealthStatus);
+router.get("/health/analysis", checkAnalysisServiceHealth);
+
 export default router;
