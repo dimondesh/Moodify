17aa890a974b39b1028f54d32dd68a30e1524adc
dimondesh <dimondesh1337@gmail.com>
2026-03-06 02:06:37 +0200

    autodeploy fix

--- frontend/src/components/ui/ShareDialog.tsx
+++ frontend/src/components/ui/ShareDialog.tsx
@@ -1,5 +1,5 @@
 // src/components/ui/ShareDialog.tsx
-import React, { useEffect, useState } from "react"; // Добавляем useState
+import React, { useEffect, useState } from "react";
 import {
   Dialog,
   DialogContent,
