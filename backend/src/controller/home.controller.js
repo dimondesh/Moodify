c43421c28daa7fa8cc30f388868622ca21a71200
dimondesh <dimondesh1337@gmail.com>
2026-04-29 23:39:49 +0300

    more optimization

--- backend/src/controller/home.controller.js
+++ backend/src/controller/home.controller.js
@@ -1,5 +1,3 @@
-// backend/src/controller/home.controller.js
-
 import mongoose from "mongoose";
 import {
   getQuickPicks,
@@ -183,6 +181,7 @@ async function getOptimizedLibrarySummary(userId) {
         localField: "albums.albumId",
         foreignField: "_id",
         as: "albumDetails",
+        pipeline: [{ $project: { title: 1, artist: 1, imageUrl: 1, type: 1 } }],
       },
     },
     {
@@ -192,16 +191,7 @@ async function getOptimizedLibrarySummary(userId) {
         foreignField: "_id",
         as: "songDetails",
         pipeline: [
-          {
-            $project: {
-              title: 1,
-              artist: 1,
-              albumId: 1,
-              imageUrl: 1,
-              duration: 1,
-              playCount: 1,
-            },
-          },
+          { $project: { title: 1, artist: 1, imageUrl: 1, duration: 1 } },
         ],
       },
     },
@@ -211,6 +201,9 @@ async function getOptimizedLibrarySummary(userId) {
         localField: "playlists.playlistId",
         foreignField: "_id",
         as: "playlistDetails",
+        pipeline: [
+          { $project: { title: 1, owner: 1, imageUrl: 1, isPublic: 1 } },
+        ],
       },
     },
     {
@@ -219,6 +212,7 @@ async function getOptimizedLibrarySummary(userId) {
         localField: "followedArtists.artistId",
         foreignField: "_id",
         as: "artistDetails",
+        pipeline: [{ $project: { name: 1, imageUrl: 1 } }],
       },
     },
     {
@@ -227,6 +221,9 @@ async function getOptimizedLibrarySummary(userId) {
         localField: "savedMixes.mixId",
         foreignField: "_id",
         as: "mixDetails",
+        pipeline: [
+          { $project: { name: 1, imageUrl: 1, sourceName: 1, type: 1 } },
+        ],
       },
     },
     {
@@ -235,66 +232,7 @@ async function getOptimizedLibrarySummary(userId) {
         localField: "savedPersonalMixes.personalMixId",
         foreignField: "_id",
         as: "personalMixDetails",
-      },
-    },
-    {
-      $lookup: {
-        from: "songs",
-        localField: "mixDetails.songs",
-        foreignField: "_id",
-        as: "mixSongs",
-        pipeline: [
-          {
-            $lookup: {
-              from: "artists",
-              localField: "artist",
-              foreignField: "_id",
-              as: "artist",
-              pipeline: [{ $project: { name: 1, imageUrl: 1 } }],
-            },
-          },
-          { $unwind: "$artist" },
-          {
-            $project: {
-              title: 1,
-              artist: 1,
-              albumId: 1,
-              imageUrl: 1,
-              duration: 1,
-              playCount: 1,
-            },
-          },
-        ],
-      },
-    },
-    {
-      $lookup: {
-        from: "songs",
-        localField: "personalMixDetails.songs",
-        foreignField: "_id",
-        as: "personalMixSongs",
-        pipeline: [
-          {
-            $lookup: {
-              from: "artists",
-              localField: "artist",
-              foreignField: "_id",
-              as: "artist",
-              pipeline: [{ $project: { name: 1, imageUrl: 1 } }],
-            },
-          },
-          { $unwind: "$artist" },
-          {
-            $project: {
-              title: 1,
-              artist: 1,
-              albumId: 1,
-              imageUrl: 1,
-              duration: 1,
-              playCount: 1,
-            },
-          },
-        ],
+        pipeline: [{ $project: { name: 1, imageUrl: 1 } }],
       },
     },
     {
@@ -303,14 +241,17 @@ async function getOptimizedLibrarySummary(userId) {
         localField: "savedGeneratedPlaylists.playlistId",
         foreignField: "_id",
         as: "genPlaylistDetails",
+        pipeline: [{ $project: { nameKey: 1, imageUrl: 1 } }],
       },
     },
+
     {
       $lookup: {
         from: "artists",
         localField: "albumDetails.artist",
         foreignField: "_id",
         as: "albumArtists",
+        pipeline: [{ $project: { name: 1, imageUrl: 1 } }],
       },
     },
     {
@@ -319,6 +260,7 @@ async function getOptimizedLibrarySummary(userId) {
         localField: "songDetails.artist",
         foreignField: "_id",
         as: "songArtists",
+        pipeline: [{ $project: { name: 1, imageUrl: 1 } }],
       },
     },
     {
@@ -327,6 +269,7 @@ async function getOptimizedLibrarySummary(userId) {
         localField: "playlistDetails.owner",
         foreignField: "_id",
         as: "playlistOwners",
+        pipeline: [{ $project: { fullName: 1, imageUrl: 1 } }],
       },
     },
 
@@ -516,13 +459,6 @@ async function getOptimizedLibrarySummary(userId) {
                       in: "$$libItem.addedAt",
                     },
                   },
-                  songs: {
-                    $filter: {
-                      input: "$mixSongs",
-                      as: "song",
-                      cond: { $in: ["$$song._id", "$$mix.songs"] },
-                    },
-                  },
                 },
               ],
             },
@@ -558,13 +494,6 @@ async function getOptimizedLibrarySummary(userId) {
                         },
                       },
                       in: "$$libItem.addedAt",
-                    },
-                  },
-                  songs: {
-                    $filter: {
-                      input: "$personalMixSongs",
-                      as: "song",
-                      cond: { $in: ["$$song._id", "$$personalMix.songs"] },
                     },
                   },
                 },
